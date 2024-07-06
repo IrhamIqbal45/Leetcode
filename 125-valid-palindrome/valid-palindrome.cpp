49de0abd071a15dcbc137af8ff920b9914b9c5a2
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-07 03:25:20 +0500

    Time: 3 ms (89.02%) | Memory: 8.7 MB (43.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+    //     bool pal=true;
+    //     string res;
+    //     for(char v:s)
+    //     {
+    //         if(isalnum(v))
+    //         {
+    //             res += tolower(v);
+    //         }
+    //     }
+    //     int i=0;
+    //     int j=res.size()-1;
+    //     while(i<=j)
+    //     {
+    //         if(res[i++]!=res[j--])
+    //         {
+    //             pal=false;
+    //         }
+    //     }
+    //     return pal;
+    // }
         int first =0;
         int end = s.size()-1 ;
         while(first<end){

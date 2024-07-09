3d56b5fe2187d49ef64bf080ee184b535dcccc6e
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-09 23:23:43 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (20.22%) - LeetSync

---
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfChild(int n, int k) {
+        int pos=0;
+        int dir=1;
+        for(int i=0;i<k;i++)
+        {
+            pos += dir;
+            if(pos==0 || pos == n-1)
+            {
+                dir = -dir;
+            }
+        }
+        return pos;
+    }
+};

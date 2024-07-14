015da01cfdd0333f0b1ee92acae72b737184a22a
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-15 02:17:02 +0500

    Time: 3 ms (44.20%) | Memory: 7.7 MB (16.08%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    vector<double> convertTemperature(double cel) {
-        vector<double>ans;
-        ans.push_back(cel + 273.15);
-        ans.push_back(cel * 1.80 + 32.00);
-        return ans;
+    vector<double> convertTemperature(double celsius) {
+        // vector<double>ans;
+        // ans.push_back(cel + 273.15);
+        // ans.push_back(cel * 1.80 + 32.00);
+        // return ans;
+        double kelvin=celsius+273.15;
+        double fahren=celsius*1.80+32.00;
+        return {kelvin,fahren};
     }
 };

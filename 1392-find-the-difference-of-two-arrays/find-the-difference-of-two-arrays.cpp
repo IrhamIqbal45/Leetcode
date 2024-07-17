a4547ea2d360da1366fab0f2a5940454feaf72a6
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-17 13:53:27 +0500

    Time: 46 ms (39.68%) | Memory: 36.3 MB (58.54%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -2,23 +2,50 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         // unordered_map<int,int>mp;
-        // for()
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
-        
-        vector<int> distinct_nums1, distinct_nums2;
-        for (int num : set1) {
-            if (set2.count(num) == 0) {
-                distinct_nums1.push_back(num);
-            }
+        // unordered_map<int,int>np;        
+        vector<int>ans,mas;
+        vector<vector<int>>a;
+        // for(auto v:nums1)
+        // {
+        //     mp[v]++;
+        // }
+        // for(auto v:nums2)
+        // {
+        //     np[v]++;
+        // }
+        // for(auto v:nums2)
+        // {
+        //     mp[v]--;
+        // }
+        // for(auto v:nums1)
+        // {
+        //     np[v]--;
+        // }
+        // for(auto v:mp)
+        // {
+        //     ans.push_back(v.first);
+        // }
+        // for(auto v:np)
+        // {
+        //     mas.push_back(v.first);
+        // }
+        // a.push_back(ans);
+        // a.push_back(mas);
+        // return a;
+    set<int> set1(nums1.begin(), nums1.end());
+    set<int> set2(nums2.begin(), nums2.end());
+    for (int num : set1) {
+        if (set2.find(num) == set2.end()) {
+            ans.push_back(num);
         }
-
-        for (int num : set2) {
-            if (set1.count(num) == 0) {
-                distinct_nums2.push_back(num);
-            }
+    }
+    for (int num : set2) {
+        if (set1.find(num) == set1.end()) {
+            mas.push_back(num);
         }
-
-        return {distinct_nums1, distinct_nums2};
+    }
+    a.push_back(ans);
+    a.push_back(mas);
+    return a;
     }
 };

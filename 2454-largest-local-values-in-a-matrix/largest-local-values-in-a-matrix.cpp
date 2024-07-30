6aa7898823bf9d11f19a5c932f7d26dd5b05030c
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-30 11:49:13 +0500

    Time: 14 ms (21.97%) | Memory: 13.8 MB (38.52%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -5,30 +5,35 @@ public:
         int m = grid[0].size();
         vector<vector<int>>res(n-2, vector<int>(m-2));
 
-        auto findlargest = [&grid](int row,int col){
-            int best = grid[row][col];
-            for(int i=row;i<row+3;i++)
-            {
-                for(int j=col;j<col+3;j++)
-                {
-                    best = max(best,grid[i][j]);
-                }
-            }
+        // auto findlargest = [&grid](int row,int col){
+        //     int best = grid[row][col];
+        //     for(int i=row;i<row+3;i++)
+        //     {
+        //         for(int j=col;j<col+3;j++)
+        //         {
+        //             best = max(best,grid[i][j]);
+        //         }
+        //     }
 
-            return best;
-        };
-
-
+        //     return best;
+        // };
 
         for(int i=0;i<n-2;i++)
         {
             for(int j=0;j<grid[i].size()-2;j++)
             {
-                res[i][j] = findlargest(i,j);
+                int a=0;
+               for(int k=i;k<i+3;k++)
+               {
+                for(int r=j;r<j+3;r++)
+                {
+                    a=max(a,grid[k][r]);
+                }
+               }
+               res[i][j]=a;
             }
         }
-
-
         return res;
+        
     }
 };

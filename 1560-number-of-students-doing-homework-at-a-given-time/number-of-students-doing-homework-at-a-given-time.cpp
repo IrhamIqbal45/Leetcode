47cf0a32a5994078b55aa572a6b82cdc0c5a742a
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-07-30 12:13:48 +0500

    Time: 3 ms (56.09%) | Memory: 13.6 MB (9.70%) - LeetSync

--- 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.cpp
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-        int count=0;
-        int r=0;
-        while(r<startTime.size())
+        // int count=0;
+        // int r=0;
+        // while(r<startTime.size())
+        // {
+        //     for(int i=startTime[r];i<=endTime[r];i++)
+        //     {
+        //         if(i == queryTime)
+        //         count++;
+        //     }
+        //     r++;
+        // }
+        // return count;
+        int res=0;
+        for(int i=0;i<startTime.size();++i)
         {
-            for(int i=startTime[r];i<=endTime[r];i++)
-            {
-                if(i == queryTime)
-                count++;
-            }
-            r++;
+            if(startTime[i]<=queryTime && queryTime<=endTime[i])
+            res++;
         }
-        return count;
+        return res;
     }
 };
